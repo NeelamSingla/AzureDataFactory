e810f83014c9803d6ffdfcb45c8fea28b80ed649
NSingla <35268654+NeelamSingla@users.noreply.github.com>
2019-05-02 14:22:16 +0200

    Add files via upload

---
+++ CreateTable_demo.sql
@@ -0,0 +1,15 @@
+CREATE TABLE MockPersonData (
+    Id int,
+    first_name varchar(255),
+    last_name varchar(255),
+    email varchar(100),
+    gender varchar(20),
+    ip_address varchar(100)
+);
+
+Create Table MetaData
+(
+updateddate Date
+);
+
+Insert Into MetaData (updateddate) values ('2019-05-07 13:02:49.147')
